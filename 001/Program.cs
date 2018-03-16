47f60082eef7eb653958de49f5619b2d0a82fdb4
theacid <boulderdash@outlook.com>
2018-03-16 21:13:12 +0100

    Add 001

---
+++ 001/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+
+            Console.WriteLine("Enter number: ");
+            value = int.Parse(Console.ReadLine());
+
+            if(value % 2 == 0) {
+                Console.WriteLine("Value is even.");
+            }
+            else {
+                Console.WriteLine("Value is odd.");
+            }
+        }
+    }
+}
