8885ecb82ca3e97895b9b37d86739b4cc3911fbd
theacid <boulderdash@outlook.com>
2018-03-16 21:30:26 +0100

    Add 002

---
+++ 002/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstValue, secondValue, swapper;
+            Console.Write("First value:");
+            firstValue = int.Parse(Console.ReadLine());
+            
+            Console.Write("Second value:");
+            secondValue = int.Parse(Console.ReadLine());
+            
+            swapper = firstValue;
+            firstValue = secondValue;
+            secondValue = swapper;
+
+            Console.Write("After swapping two values:");
+            Console.Write($"\nFirst value: {firstValue}. Second value: {secondValue}\n");
+        }
+    }
+}
