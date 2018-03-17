781871e01abe82ba60f5ac34b5d0e6554e39493d
theacid <boulderdash@outlook.com>
2018-03-17 18:22:09 +0100

    Added 004

---
+++ 004/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value, rev, sum = 0;
+
+            Console.WriteLine("Enter a value: ");
+            value = int.Parse(Console.ReadLine());
+
+            while(value != 0) {
+                rev = value % 10;
+                value = value / 10;
+                sum = sum + rev;
+            }
+
+            Console.WriteLine($"Sum of digits: {sum}");
+        }
+    }
+}
