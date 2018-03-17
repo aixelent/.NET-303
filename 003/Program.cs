a2a5b86a5793a4bf7a4be5f8fb889a7d33f65284
theacid <boulderdash@outlook.com>
2018-03-17 16:21:07 +0100

    Add 003

---
+++ 003/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstValue, secondValue;
+            
+            Console.Write("First value: ");
+            firstValue = int.Parse(Console.ReadLine());
+            Console.Write("Second value: ");
+            secondValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Before: {firstValue}, {secondValue}");
+
+            firstValue = firstValue + secondValue;
+            secondValue = firstValue - secondValue;
+            firstValue = firstValue - secondValue;
+
+            Console.WriteLine($"After: {firstValue}, {secondValue}");
+        }
+    }
+}
