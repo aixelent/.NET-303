aeb472e1372d8e7e40f98af51e94e9fedf20e31e
theacid <boulderdash@outlook.com>
2018-03-17 18:40:24 +0100

    Added 005

---
+++ 005/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value, reverse = 0;
+
+            Console.WriteLine("Enter a value: ");
+            value = int.Parse(Console.ReadLine());
+
+            while(value != 0) {
+                reverse = reverse * 10;
+                reverse = reverse + value % 10;
+                value = value/10;
+            }
+
+            Console.WriteLine($"Reversed value: {reverse}");
+        }
+    }
+}
