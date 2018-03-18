59d53af00b9f368772dfeffc071fb3460cf4dba7
theacid <boulderdash@outlook.com>
2018-03-18 21:49:13 +0100

    Added 006

---
+++ 006/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Generating number from 0 to 100: " + random.Next(0, 100));
+        }
+    }
+}
