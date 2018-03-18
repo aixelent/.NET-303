f4c87e07246b5ece2ffaf91137bc56b19b104ff3
theacid <boulderdash@outlook.com>
2018-03-18 22:07:35 +0100

    Added 007

---
+++ 007/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Random static generator: ");
+            RandomStat();
+            Console.WriteLine("And again for other result: ");
+            RandomStat();
+        }
+
+        static Random _random = new Random();
+        static void RandomStat() 
+        {
+            int num = _random.Next();
+            Console.WriteLine(num);
+        }
+    }
+}
