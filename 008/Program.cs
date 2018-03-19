e1b58ddcd248e320a1bc71c92f5f3bab6c6dd64d
theacid <boulderdash@outlook.com>
2018-03-19 22:58:15 +0100

    Added 008

---
+++ 008/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _008
+{
+    class Program
+    {
+        int year;
+        public void getYear() {
+            // int year;
+            Console.WriteLine("Enter year: ");
+            year = Convert.ToInt32(Console.ReadLine());
+        }
+         
+        public void isLeap() {
+            if(year % 400 == 0 || ((year % 4 == 0) && (year % 100 != 0))) {
+                Console.WriteLine("{0} is a leap year", year);
+            }
+            else {
+                Console.WriteLine("{0} isn't a leap year", year);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program leapYear = new Program();
+            leapYear.getYear();
+            leapYear.isLeap();
+        }
+    }
+}
