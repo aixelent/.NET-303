3e00d2b9dd9d22e8660b2f681bf74731dec8080a
theacid <boulderdash@outlook.com>
2018-03-20 19:52:59 +0100

    Updated 010 structure -> 010 is now 10.1

---
+++ 010/10.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime firstDate = new DateTime(2017, 3, 20);
+            Console.WriteLine("First date: {0}", firstDate);
+
+            DateTime secondDate = new DateTime(2018, 5, 15);
+            Console.WriteLine("Second date: {0}", secondDate);
+
+            if(firstDate < secondDate) {
+                Console.WriteLine("{0} is earlier than {1}", firstDate, secondDate);
+            }
+            else {
+                Console.WriteLine("{0} is after {1}", firstDate, secondDate);
+            }
+        }
+    }
+}
