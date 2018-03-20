3228e812d20299963f755e081134db080580fcf3
theacid <boulderdash@outlook.com>
2018-03-20 22:40:23 +0100

    Added 011

---
+++ 011/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int fValue = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second value: ");
+            int sValue = Convert.ToInt32(Console.ReadLine());
+
+            //Add
+            Console.WriteLine("{0} + {1} = {2}", fValue, sValue, fValue + sValue);
+            //Substract
+            Console.WriteLine("{0} - {1} = {2}", fValue, sValue, fValue - sValue);
+            //Multiply
+            Console.WriteLine("{0} * {1} = {2}", fValue, sValue, (fValue * sValue));
+            //Divide
+            Console.WriteLine("{0} / {1} = {2}", fValue, sValue, (fValue / sValue));
+        }
+    }
+}
