323014956aae0675d5b95c6183bcb5bb7cb0256d
theacid <boulderdash@outlook.com>
2018-03-21 22:40:48 +0100

    Added 014

---
+++ 014/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("First value: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second value: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            
+            if(a >= 0 && b >= 0) {
+                Console.WriteLine("Both values are positive");
+            } 
+            else {
+                Console.WriteLine("One of values are negative");
+            }
+        }
+    }
+}
