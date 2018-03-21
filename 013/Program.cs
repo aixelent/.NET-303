923c076cf771b8de71fa24400a274f9ec57f354f
theacid <boulderdash@outlook.com>
2018-03-21 23:51:35 +0100

    Added 013

---
+++ 013/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inp;
+            Console.WriteLine("Type keyword: ");
+            inp = Convert.ToString(Console.ReadLine());
+
+            for(int i = 0; i < inp.Length; i++) {
+                Console.Write(inp[inp.Length - i - 1]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
