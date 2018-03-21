f64c8e713390b8443e02d756a0162a8030c4fab1
theacid <boulderdash@outlook.com>
2018-03-21 23:03:59 +0100

    Added 015

---
+++ 015/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("First value: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second value: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if(a == b) {
+                Console.WriteLine("(({0} + {1}) * 9) = {2}", a, b, ((a + b) * 9));
+            }
+            else {
+                Console.WriteLine("{0} + {1} = {2}", a, b, (a + b));
+            }
+        }
+    }
+}
