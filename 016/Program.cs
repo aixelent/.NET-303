7279d84b7dd9ea9206ecebc88ab429ac51bdadc5
theacid <boulderdash@outlook.com>
2018-03-27 21:57:29 +0200

    016

---
+++ 016/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd numbers from 0 to 999:");
+
+            for(int i = 0; i <= 999; i++) {
+                if(i % 2 != 0) {
+                    Console.WriteLine(i.ToString());
+                }
+            }
+        }
+    }
+}
