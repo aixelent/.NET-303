006eb6571e532f988e9f4fcc6833106f2b8e08bb
theacid <boulderdash@outlook.com>
2018-03-29 13:14:28 +0200

    Added 020.1

---
+++ 020/20.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _20._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reverse string: ");
+            string s = Console.ReadLine();
+            string[] str = s.Split("");
+
+            foreach(String st in str.Reverse()) {
+                Console.Write(s + "");
+            }
+
+
+            // string s;
+            // Console.WriteLine("Reverse string; ");
+            // s = Console.ReadLine();
+
+            // Console.WriteLine("Reversed: " + String.Join("", s.Split("").Reverse()));           
+        }
+    }
+}
