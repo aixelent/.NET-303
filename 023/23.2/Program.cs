0b6aac1a1ff80b7faca352d009434dc5bfc8f387
theacid <boulderdash@outlook.com>
2018-03-30 14:48:00 +0200

    Added 023 second example

---
+++ 023/23.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _23._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hexadecimal to decimal converter. Type a number: ");
+            int inputValue = int.Parse(Console.ReadLine());
+            int binParse = int.Parse(Convert.ToString(inputValue, 2));
+            Console.WriteLine("Binary: " + binParse);
+        }
+    }
+}
