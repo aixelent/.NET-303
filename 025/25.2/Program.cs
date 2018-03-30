b85856b13c62e8bdaac784410c078dff1ef8b3f3
theacid <boulderdash@outlook.com>
2018-03-30 19:38:53 +0200

    Added 025.2 as a second example

---
+++ 025/25.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _25._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a string: ");
+            string inputValue = Console.ReadLine();
+            Console.WriteLine(inputValue.Substring(0) == "Bulbasaur");
+        }
+    }
+}
