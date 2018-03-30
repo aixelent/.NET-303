c2e761fe51cf296c8b9a14e15d44a4c57b86f55e
theacid <boulderdash@outlook.com>
2018-03-30 14:47:19 +0200

    Added 023

---
+++ 023/23.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Decimal to binary converter. Type a number: ");
+            Console.WriteLine(Convert.ToString(Convert.ToInt32("Binary: " + Console.ReadLine()), 2));
+        }
+    }
+}
