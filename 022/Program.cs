8550427c8808d9edb321f8f70e033f54f959f707
theacid <boulderdash@outlook.com>
2018-03-30 14:45:58 +0200

    Added 022

---
+++ 022/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputValue;
+            Console.WriteLine("Hexadecimal to decimal converter. Type a number: ");
+            inputValue = Convert.ToString(Console.ReadLine());
+            int decimalValue = int.Parse(inputValue, System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine("Decimal: " + decimalValue);
+        }
+    }
+}
