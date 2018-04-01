7f73b493b67156687a2cb9c20e32b6bf0ecfa8e4
theacid <boulderdash@outlook.com>
2018-04-01 16:26:27 +0200

    Added 026

---
+++ 026/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _026
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value: ");
+            int inputValue = Convert.ToInt32(Console.ReadLine());
+
+            if(inputValue > 0) {
+                if(inputValue % 3 == 0 || inputValue % 5 == 0) {
+                    Console.WriteLine("Value {0} is multiple by 3 or 5", inputValue);
+                }
+                else {
+                    Console.WriteLine("{0} is not a multiple.", inputValue);
+                }
+            }
+        }
+    }
+}
