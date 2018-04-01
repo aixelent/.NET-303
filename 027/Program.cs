29e1a133548b42f19a74e00fd256dd525d0e78ff
theacid <boulderdash@outlook.com>
2018-04-01 16:26:38 +0200

    Added 027

---
+++ 027/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _027
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First value: ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("First second: ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("First third: ");
+            int third = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("First fourth: ");
+            int fourth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("First fourth: ");
+            
+            Console.WriteLine("Largest value: " + Math.Max(first, Math.Max(second, Math.Max(third, fourth)))  + "\n" 
+                + "Lowest value: " + Math.Min(first, Math.Min(second, Math.Min(third, fourth)))); 
+        }
+    }
+}
